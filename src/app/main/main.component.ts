ed967f6a780be6b20bff892d8e9ce6be7f9468aa
ignacio <iaraya19@hotmail.com>
2021-06-06 22:35:09 -0400

    Updates

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Tone from 'tone';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  synth : any;
+  msdown : boolean = false;
+
+  constructor() {
+    this.synth = new Tone.Synth().toDestination();
+  }
 
   ngOnInit(): void {
+  }
+
+  msover(note : string) {
+    if (this.msdown) {
+      this.play(note);
+    }
+  }
+
+  play(note :string) {
+    this.synth.triggerAttackRelease(note, '8n');
   }
 
 }
